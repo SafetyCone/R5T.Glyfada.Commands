7a6c1c18c5f6b91b38151dc1abfb6e4e2c6ae8ac
MinexAutomation <minex.automation@gmail.com>
2019-12-12 22:56:20 -0800

    Added git-init -quiet (-q) methods.

---
+++ source/R5T.Glyfada.Commands/Code/Context Extensions/GitInitContextExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using R5T.Heraklion;
+
+
+namespace R5T.Glyfada.Commands
+{
+    public static class GitInitContextExtensions
+    {
+        public static ICommandBuilderContext<GitInitContext> SetQuietShort(this ICommandBuilderContext<GitInitContext> gitInitContext)
+        {
+            gitInitContext.CommandBuilder.Append("-q");
+
+            return gitInitContext;
+        }
+
+        public static ICommandBuilderContext<GitInitContext> SetQuietLong(this ICommandBuilderContext<GitInitContext> gitInitContext)
+        {
+            gitInitContext.CommandBuilder.Append("-quiet");
+
+            return gitInitContext;
+        }
+
+        public static ICommandBuilderContext<GitInitContext> SetQuiet(this ICommandBuilderContext<GitInitContext> gitInitContext)
+        {
+            gitInitContext.SetQuietLong();
+
+            return gitInitContext;
+        }
+    }
+}
