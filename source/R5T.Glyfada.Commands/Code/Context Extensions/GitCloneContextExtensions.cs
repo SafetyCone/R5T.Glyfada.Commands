d90c64a0594db3576e1c7417bbb131f42f936516
MinexAutomation <minex.automation@gmail.com>
2019-12-14 13:45:52 -0800

    Added git-clone context extensions.

---
+++ source/R5T.Glyfada.Commands/Code/Context Extensions/GitCloneContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Heraklion;
+using R5T.Piraeus;
+
+
+namespace R5T.Glyfada.Commands
+{
+    public static class GitCloneContextExtensions
+    {
+        public static ICommandBuilderContext<GitCloneContext> SetRepository(this ICommandBuilderContext<GitCloneContext> gitCloneContext, string repositoryURL)
+        {
+            gitCloneContext.CommandBuilder.Append(repositoryURL);
+
+            return gitCloneContext;
+        }
+
+        public static ICommandBuilderContext<GitCloneContext> SetDirectory(this ICommandBuilderContext<GitCloneContext> gitCloneContext, string directoryPath)
+        {
+            gitCloneContext.CommandBuilder.AppendPath(directoryPath);
+
+            return gitCloneContext;
+        }
+
+        public static ICommandBuilderContext<GitCloneContext> SetProgress(this ICommandBuilderContext<GitCloneContext> gitCloneContext)
+        {
+            gitCloneContext.CommandBuilder.Append("--progress");
+
+            return gitCloneContext;
+        }
+    }
+}
