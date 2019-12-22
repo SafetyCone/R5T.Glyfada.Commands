fed0be8182d3a854b10978b146f2ef82a8eca753
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:48:56 -0800

    Added Config context and context extensions.

---
+++ source/R5T.Glyfada.Commands/Code/Context Extensions/GitConfigContextExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using R5T.Heraklion;
+using R5T.Heraklion.Extensions;
+
+
+namespace R5T.Glyfada.Commands
+{
+    public static class GitConfigContextExtensions
+    {
+        public static ICommandBuilderContext<GitConfigContext> Get(this ICommandBuilderContext<GitConfigContext> gitConfigContext, string parameterName)
+        {
+            gitConfigContext
+                .AppendNameValuePair("--get", parameterName)
+                ;
+
+            return gitConfigContext;
+        }
+
+        public static ICommandBuilderContext<GitConfigContext> GetRemoteOriginUrl(this ICommandBuilderContext<GitConfigContext> gitConfigContext)
+        {
+            gitConfigContext.Get("remote.origin.url");
+
+            return gitConfigContext;
+        }
+
+        public static ICommandBuilderContext<GitConfigContext> GetRemoteRepositoryUrl(this ICommandBuilderContext<GitConfigContext> gitConfigContext)
+        {
+            gitConfigContext.GetRemoteOriginUrl();
+
+            return gitConfigContext;
+        }
+    }
+}
